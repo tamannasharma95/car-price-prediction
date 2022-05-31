b2e6b12acf557c6da567259f14fdd44bf9596eb8
tamma <tamannasharma95@yahoo.com>
2022-05-31 23:49:57 +0530

    added os import for the os module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import jsonify
 import requests
 import pickle
+import os 
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
